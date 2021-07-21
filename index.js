8325636e9c4553082fee217d5104a73c9322c2f8
davitsong <s01068123488@gmail.com>
2021-07-21 13:14:04 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,14 +2,39 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const {User} = require("./User")
+const bodyParser = require('body-parser')
+
+const config = require("./config/key")
+
+app.use(bodyParser.urlencoded({extended: true  }))
+
+
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://songbangwon:01068123488@cluster0.3jq85.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MonggoDB connected...'))
   .catch(err => console.log(err))
 
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldsdasdasdasd!')
+})
+
+app.post('/register',(req,res)=>{
+  
+  const user = new User(req.body)
+  
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
