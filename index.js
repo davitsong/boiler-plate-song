4a8009e96bc8c2c9ae2a5443e45124c58be159bd
davitsong <s01068123488@gmail.com>
2021-10-29 17:12:51 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')         //express module 가져옴
 const app = express()
 const port = 5000                          //5000번 서버 
-
-const {User} = require("./User")
+const {User} = require("./models/User")
 const bodyParser = require('body-parser')
-
 const config = require("./config/key")
 
-app.use(bodyParser.urlencoded({extended: true  }))
 
+//application /x-www-form-urlencoded 분서해서 가져옴
+app.use(bodyParser.urlencoded({extended: true  }))            
 
+//application/jason 타입을 분석해서 가져옴 
 app.use(bodyParser.json())
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://songbangwon:01068123488@cluster0.3jq85.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -20,17 +21,19 @@ mongoose.connect('mongodb+srv://songbangwon:01068123488@cluster0.3jq85.mongodb.n
 
 
 
-
 app.get('/', (req, res) => {
-  res.send('Hello Worldsdasdasdasdsdfsassfgsd!')
+  res.send('Hello World!aaf')
 })
 
 app.post('/register',(req,res)=>{      
-  
+   
+
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다
   const user = new User(req.body)
   
-  user.save((err,userInfo)=>{
-    if(err) return res.json({success: false, err})
+  user.save((err,userInfo)=>{                         //정보들이 user모델에 저장                      
+    if(err) return res.json({success: false, err})    //성공여부를 jason으로 저장
     return res.status(200).json({
       success: true
     })
