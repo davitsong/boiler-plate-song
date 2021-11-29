999f5164a7e8c492186b2989691438244e822c60
davitsong <s01068123488@gmail.com>
2021-11-29 13:36:01 +0900

    nodejs register login logout

--- models/User.js
+++ models/User.js
@@ -77,7 +77,7 @@ userSchema.methods.generateToken = function(cb){
     console.log('user._id',user._id)
 
     // jsonwebtoken을 사용하여 토큰 생성하기
-    var token =jwt.sign(user._id.toHexString(), 'secretToekn')
+    var token =jwt.sign(user._id.toHexString(), 'secretToken')
 
     user.token = token
     user.save(function(err,user){
@@ -86,6 +86,24 @@ userSchema.methods.generateToken = function(cb){
     })
 
     //user._id + 'secretToken' = token 
+
+}
+
+userSchema.statics.findByToken = function(token,cb){
+    var user = this;
+
+    //user._id + '' = token
+    //토큰을 decode
+    jwt.verify(token,'secretToken',function(err,decoded){
+        //유저아이디를 이용하여 유저를 찾고
+        //클라이언트에서 가져온토큰과 데이터베이스에 저장된 토큰이 일치하는지 
+        //확인
+
+        user.findOne({"_id":decoded, "token":token}, function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
 
 }
 
