012f89639497c9483f7cd2beda201104f87183ad
davitsong <s01068123488@gmail.com>
2021-11-30 20:38:55 +0900

    proxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import {
 } from "react-router-dom";
 
 import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from "./components/views/LoginPage/LoginPage";
-import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
